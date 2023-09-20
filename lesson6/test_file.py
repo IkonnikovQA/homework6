0e67fe08b53cf7d23f34b6b75a5651e2c41fe5f0
Ikonnikov <Ikonnikov47@gmail.com>
2023-09-20 17:43:41 +0500

    test file

--- lesson6/test_file.py
+++ lesson6/test_file.py
@@ -1,6 +1,18 @@
+import os.path
+
 import requests
 
-url = 'https://selenium.dev/images/selenium_logo_square.png'
+url = 'https://selenium.dev/images/selenium_logo_square_green.png'
 
 response = requests.get(url)
+
+with open('selenium_logo_square_green.png', 'wb').write(response.content) as file:
+    file.write(response.content)
+
+with open('selenium_logo_square_green.png', 'rb') as file:
+    selenium_png = file.read()
+    print(len(selenium_png))
+    assert len(selenium_png) == 30803
+
+print(os.path.getsize('selenium_logo_square_green.png'))
 
